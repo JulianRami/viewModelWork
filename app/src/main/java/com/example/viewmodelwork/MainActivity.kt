ae6476bff2b6e590a8b87b0114a7f720f7365712
JulianRami <julian.ramirez04@uptc.edu.co>
2024-09-03 10:38:01 -0500

    feat: add uiState and ViewModel

--- app/src/main/java/com/example/viewmodelwork/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelwork/MainActivity.kt
@@ -1,92 +1,41 @@
 package com.example.viewmodelwork
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.lifecycleScope
-import com.example.viewmodelwork.databinding.ActivityMainBinding
-import kotlinx.coroutines.launch
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private val viewmodel: WorkViewModel by viewModels()
+    private val viewModel: PostsViewModel by viewModels()
+    private lateinit var rvPosts: RecyclerView
+    private val postList = arrayListOf<Post>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Inflate viewBinding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        initUIStateLifecycle()
-        initListeners()
+        setContentView(R.layout.activity_main)
+        initViews()
+        observeViewModel()
     }
 
-    private fun initListeners() {
-        binding.btnRollDice.setOnClickListener {
-            viewmodel.rollDice()
+    private fun initViews() {
+        rvPosts = findViewById(R.id.rv_posts)
+        rvPosts.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = RVAdapterPosts(postList)
         }
     }
 
-    private fun initUIStateLifecycle() {
-        // Once the activity has a lifecycle the code inside launch is executed
-        lifecycleScope.launch {
-            // Collect uiState value everytime it is changed
-            viewmodel.uiState.collect { uiState ->
-                with(binding) {
-                    // Validate that diceOne is not null
-                    uiState.diceOne?.let {
-                        // Set drawable to ivDiceOne
-                        ivDiceOne.setImageDrawable(
-                            ContextCompat.getDrawable(
-                                this@MainActivity,
-                                uiState.diceOne
-                            )
-                        )
-                    }
-                    // Set drawable to ivDiceTwo
-                    ivDiceTwo.setImageDrawable(
-                        ContextCompat.getDrawable(
-                            this@MainActivity,
-                            uiState.diceTwo
-                        )
-                    )
-                    // Set text to tvRolls
-                    tvRolls.text = getString(R.string.rolls_attemps, uiState.numberOfRolls)
-                    // Validate that dicesAreEqual is not null
-                    uiState.dicesAreEqual?.let {
-                        // Using apply, we access to the internal variables inside ivDicesAreEqual
-                        ivDicesAreEqual.apply {
-                            setImageDrawable(
-                                ContextCompat.getDrawable(
-                                    this@MainActivity,
-                                    if (uiState.dicesAreEqual == true) R.drawable.ic_check else R.drawable.ic_close
-                                )
-                            )
-                            setColorFilter(
-                                ContextCompat.getColor(
-                                    this@MainActivity,
-                                    if (uiState.dicesAreEqual == true) R.color.green else R.color.red
-                                )
-                            )
-                        }
-                        // The previous code (the ivDicesAreEqual.apply { ... } is the same as the below code
-                        // Don't uncomment this code, is just an example
-//                        ivDicesAreEqual.setImageDrawable(
-//                            ContextCompat.getDrawable(
-//                                this@MainActivity,
-//                                if (uiState.dicesAreEqual == true) R.drawable.ic_check else R.drawable.ic_close
-//                            )
-//                        )
-//                        ivDicesAreEqual.setColorFilter(
-//                            ContextCompat.getColor(
-//                                this@MainActivity,
-//                                if (uiState.dicesAreEqual == true) R.color.green else R.color.red
-//                            )
-//                        )
-                    }
-                }
+    private fun observeViewModel() {
+        viewModel.uiState.observe(this) { uiState ->
+            uiState.posts.let {
+                rvPosts.adapter = RVAdapterPosts(it)
             }
         }
     }
+
 }
